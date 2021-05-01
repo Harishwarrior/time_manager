2c12d07d7688a510ba4129068cefda7929e16dcc
harishwarrior <warriorharish95668@gmail.com>
2021-05-01 15:10:31 +0530

    added autosave on backpress

--- lib/screens/add_task_view.dart
+++ lib/screens/add_task_view.dart
@@ -18,86 +18,95 @@ class _AddTaskState extends State<AddTask> {
     tasksBox.add(task);
   }
 
+  Future<bool> saveTask() async {
+    if (_formKey.currentState!.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+      _formKey.currentState!.save();
+      final newTask = Task(_title, _duration, _description);
+      addTask(newTask);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Saved')));
+      return true;
+    }
+    ;
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_formKey.currentState!.validate()) {
-            // If the form is valid, display a snackbar. In the real world,
-            // you'd often call a server or save the information in a database.
-            _formKey.currentState!.save();
-            final newTask = Task(_title, _duration, _description);
-            addTask(newTask);
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text('Saved')));
-          }
-          ;
-        },
-        child: Icon(Icons.save),
-      ),
-      appBar: AppBar(),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          margin: EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                child: TextFormField(
-                  style: TextStyle(height: 1.5, fontSize: 25.0),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some title';
-                    }
-                    return null;
-                  },
-                  textCapitalization: TextCapitalization.sentences,
-                  decoration: InputDecoration(
-                      hintText: 'Title',
-                      border: InputBorder.none,
-                      hintStyle: TextStyle(fontSize: 25)),
-                  onSaved: (value) => _title = value!,
+    return WillPopScope(
+      onWillPop: saveTask,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            saveTask();
+          },
+          child: Icon(Icons.save),
+        ),
+        appBar: AppBar(),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            margin: EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  child: TextFormField(
+                    style: TextStyle(height: 1.5, fontSize: 25.0),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some title';
+                      }
+                      return null;
+                    },
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: InputDecoration(
+                        hintText: 'Title',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(fontSize: 25)),
+                    onSaved: (value) => _title = value!,
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Slider(
-                      value: _duration,
-                      min: 0.0,
-                      max: 12.0,
-                      divisions: 24,
-                      label: '${_duration.toString() + ' hrs'}',
-                      onChanged: (double value) {
-                        setState(() {
-                          _duration = value;
-                        });
-                      },
+                Row(
+                  children: [
+                    Expanded(
+                      child: Slider(
+                        value: _duration,
+                        min: 0.0,
+                        max: 12.0,
+                        divisions: 24,
+                        label: '${_duration.toString() + ' hrs'}',
+                        onChanged: (double value) {
+                          setState(() {
+                            _duration = value;
+                          });
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the description';
-                    }
-                    return null;
-                  },
-                  textCapitalization: TextCapitalization.sentences,
-                  decoration: InputDecoration(
-                    hintText: 'Note',
-                    border: InputBorder.none,
-                  ),
-                  onSaved: (value) => _description = value!,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
+                  ],
                 ),
-              ),
-            ],
+                Expanded(
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the description';
+                      }
+                      return null;
+                    },
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: InputDecoration(
+                      hintText: 'Note',
+                      border: InputBorder.none,
+                    ),
+                    onSaved: (value) => _description = value!,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
