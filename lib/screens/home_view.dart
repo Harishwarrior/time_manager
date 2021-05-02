cf69ab90cd8cc6ec3fb7f6ade90a2a95c048eaec
harishwarrior <warriorharish95668@gmail.com>
2021-05-02 10:45:51 +0530

    added animation on page navigation

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -33,12 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddTask(),
-            ),
-          );
+          Navigator.of(context).push(_createRoute());
         },
         child: Icon(Icons.note_add_outlined),
       ),
@@ -67,6 +62,24 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => AddTask(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(1.0, 0.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
 
 Widget CustomListView(BuildContext context, int index, Task task) {
